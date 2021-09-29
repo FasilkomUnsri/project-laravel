5060cfc7604d78ec6b71144209016ebbfe23bcb3
Ahmad Peka Maryantoni <ahmadpeka6@gmail.com>
2021-09-29 20:40:02 +0700

    contoh

---
+++ code/Ahmad_Peka_Maryantoni_09020582024008/web_statis/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="style/custom.css" rel="stylesheet">
+</head>
+<body>
+    HALAMAN UTAMA
+<a href="/about" title="HALAMAN ABOUT">KE HALAM ABOUT US</a>
+</body>
+<footer>
+    <script src="style/custom.js"></script>
+</footer>
+</html>
