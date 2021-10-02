96d308ac57536a05958d55d7aa94d927f611ca56
Rayhan Mufti <rayhanmufti4@gmail.com>
2021-10-02 15:55:13 +0700

    buat folder (ulang)

--- code/Rayhan Mufti - 09020582024028/web_static/storage/framework/views/20841b7a571ed169ca3aa34f46d0081f284e564a.php
+++
@@ -1,29 +0,0 @@
-<!DOCTYPE html>
-<html lang="en"><head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-<link href="style/custom.css" rel="stylesheet">
-<style>
-    body {
-        background:black;
-        color:white;
-    }
-</style>
- 
-</head>
-<body>
-    
-    SELAMAT DATANG KE HOME 
-    
-</body>
-<footer>
-    <script src="style/custom.js"></script>
-    <script>
-        alert("Hello World !!!");
-    </script>
-</footer>
-</html>
-
-<?php /**PATH C:\Users\Andri_\Documents\GitHub\project-laravel\code\Rayhan Mufti - 09020582024028\web_static\resources\views/home.blade.php ENDPATH**/ ?>
