96489ba6d7c70cda7e886285eaf0014494ab73d3
Selamat Alfisyahrin <90509619+SelamatAlfisyahrin@users.noreply.github.com>
2021-10-05 12:31:13 +0700

    coba-latihan

---
+++ code/Selamat Alfisyahrin-09020582024020/web_statis/resources/views/index.blade.php
@@ -0,0 +1,183 @@
+<!doctype html>
+<html lang="en">style
+  <head>
+    <!-- Required meta tags -->
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    .<style>
+        section{
+            min-height: 420px;
+        }
+    </style>
+
+    <title>MY PORTFOLIO</title>
+  </head>
+  <body class="mt-4">sriwijaya
+    
+
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+  <a class="navbar-brand">Selamat Alfisyahrin</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link active" href="#"><i class="fas fa-home"></i> <span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="https://www.instagram.com/___slmt"><i class="fab fa-instagram"></i></a>
+      <a class="nav-link" href="#"><i class="fab fa-twitter"></i></a> 
+      <a class="nav-link" href="/"><i class="fas fa-ellipsis-v"></i></a> 
+    </div>
+  </div>
+</div>
+</div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container text-center">
+    <img src="foto/lol.jpg" width="25%" class="rounded-square img-thumbnail">
+    <h1 class="display-4">Selamat Alfisyahrin</h1>
+    <p class="lead">SANCTUARY</p>
+  </div>
+</div>
+
+<section id="about" class="about">
+<div class="container">
+    <div class="row mb-3">
+        <div class="col text-center ">
+            <h2>QUOTES OF THE DAY!!!</h2>
+        </div>
+    </div>
+    <div class="row justify-content-center ">
+        <div class="col-md-5 text-center">
+            <p>Hentikan Semua Ketidak Jelasan Ini Kawan,
+            Hidupkanlah Visual Studio Mu Dan Mulailah Mengcoding Bersama Ku.</p>
+        </div>
+        <div class="col-md-5 text-center">
+            <p>Sadar diri adalah vaksin terbaik untuk penyakit yang kronis</p>
+            <br>-F-
+        </div>
+    </div>
+</div>
+</section>
+
+<section id="portfolio" class="portfolio bg-light pb-4">
+<div class="container">
+    <div class="row mb-4 pt-4">
+        <div class="col text-center">
+            <h2>GALLERY</h2>
+        </div>
+    </div>
+
+    <div class="row mb-4">
+        <div class="col-md">
+           <div class="card">
+                <img src="foto/thumbs/1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                         <p class="card-text">.....Caption.....</p>
+                </div>
+            </div>
+        </div>
+            <div class="col-md">
+           <div class="card">
+                <img src="foto/thumbs/2.jpg" class="card-img-top" alt="..." >
+                <div class="card-body">
+                         <p class="card-text">.....Caption.....</p>
+                </div>
+            </div>
+        </div>
+            <div class="col-md">
+           <div class="card">
+                <img src="foto/thumbs/3.jpg" class="card-img-top" alt="..." >
+                <div class="card-body"> 
+                         <p class="card-text">.....Caption.....</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+       
+</div>
+</section>
+
+
+<section id="contact" class="contact mb-5">
+<div class="container">
+    <div class="row pt-4">
+        <div class="col text-center">
+            <h2>About The Author</h2>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-4">
+              
+                <ul class="list-group ">
+                <li class="list-group-item text-center"><h2>location</h2></li>
+                <li class="list-group-item"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d996.1752602994329!2d104.7789169!3d-2.9021980999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e3b71c1ec8e60a3%3A0x3eb2d724e546a4d7!2sRumah%20pak%20suwito!5e0!3m2!1sid!2sid!4v1633329594407!5m2!1sid!2sid"
+                    width="309" height="100" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </li>
+
+                </ul>
+        </div>
+
+
+        <div class="col-lg-6">
+            <form>
+              <div class="form-group">
+                <label for="nama">Name :</label>
+                <p class="form-group">Selamat Alfisyahrin</p>
+              </div>
+              <div class="form-group">
+                <label for="nama">Email :</label>
+                <p class="form-group">selamatalfisyahrin123@gmail.com</p>
+              </div>
+              <div class="form-group">
+                <label for="nama">Phone Number :</label>
+                <p class="form-group">+62822-6952-6669</p>
+              </div>
+              
+            </form>
+        </div>
+    </div>
+
+</div>
+</section>
+
+<footer class="bg-dark text-white ">
+    <div class="container">
+        <div class="row text-center pt-3">
+            <div class="col">
+                <p>Copyright SelamatAlfisyahrin</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
