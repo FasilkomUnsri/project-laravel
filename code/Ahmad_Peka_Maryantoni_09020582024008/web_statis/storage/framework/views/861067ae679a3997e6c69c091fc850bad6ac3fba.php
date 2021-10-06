b0b38ecdcae5eaf1076ca89cff0e8f8a24847ffb
Ahmad Peka Maryantoni <ahmadpeka6@gmail.com>
2021-10-06 22:08:43 +0700

    Latihan Webstatis 1

---
+++ code/Ahmad_Peka_Maryantoni_09020582024008/web_statis/storage/framework/views/861067ae679a3997e6c69c091fc850bad6ac3fba.php
@@ -0,0 +1,3 @@
+HALAMAN ABOUT US
+
+<a href="/" title="Halaman About"> KE HALAMAN ABOUT US</a><?php /**PATH C:\GitHub\project-laravel\code\Ahmad_Peka_Maryantoni_09020582024008\web_statis\resources\views/about_us.blade.php ENDPATH**/ ?>
---
+++ code/Ahmad_Peka_Maryantoni_09020582024008/web_statis/storage/framework/views/a4c47db68c9d0ba5fccb61f5b87c1e7d8af22df3.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\GitHub\project-laravel\code\Ahmad_Peka_Maryantoni_09020582024008\web_statis\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
