55fe70e32a1f925d329dd2d63f1f51839b0549f9
Novita01 <90509854+Novita01@users.noreply.github.com>
2021-10-06 15:51:41 +0700

    tugas1

---
+++ code/Novita_09020582024076/web_statis/resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" href="/style/custom.css">
+        <meta name="viewport" content="width=device-width , initial-scale=1.0">
+    </head>
+    
+    
+    <body>
+        
+        <div class="filter">
+        </div>
+        <div class="nav">
+              <div class="container">
+                  <h1>Anna&Jhohan</h1>
+              
+              </div>
+                
+        </div>      
+        <div class="description">
+            <h1>The Wedding</h1>
+            <p>discover our amazing website with our amazing services and contact us </p>
+        </div>
+    </body>
+</html>
