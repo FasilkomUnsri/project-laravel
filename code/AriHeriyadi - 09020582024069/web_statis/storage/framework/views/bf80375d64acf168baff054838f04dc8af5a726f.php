ef45e3c9467ad109833737949a28f368e1c285a2
Ari Heriyadi <90497439+AriHeriyadi@users.noreply.github.com>
2021-10-07 19:10:59 +0700

    ariheriyadi

---
+++ code/AriHeriyadi - 09020582024069/web_statis/storage/framework/views/bf80375d64acf168baff054838f04dc8af5a726f.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<title>Ari Melukis</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+<style>
+body {font-family: "Raleway", Arial, sans-serif}
+.w3-row img {margin-bottom: -8px}
+</style>
+<body>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-content" style="max-width:1500px">
+
+  <!-- Header -->
+  <header class="w3-container w3-xlarge w3-padding-24">
+    <a href="#" class="w3-left w3-button w3-white">Ari Melukis</a>
+    <a href="#about" class="w3-right w3-button w3-white">Welcome to our page!</a>
+  </header>
+
+  <!-- Photo Grid -->
+  <div class="w3-row">
+    <div class="w3-half">
+      <img src="IMG/1.jpg" style="width:100%">
+      <img src="IMG/2.jpg" style="width:100%">
+      <img src="IMG/3.jpg" style="width:100%">
+    </div>
+
+    <div class="w3-half">
+      <img src="IMG/4.jpg" style="width:100%">
+      <img src="IMG/5.jpg" style="width:147%">
+    </div>
+  </div>
+  
+<!-- End Page Content -->
+</div>
+
+<!-- Footer / About Section -->
+<footer class="w3-light-grey w3-padding-64 w3-center" id="about">
+  <h2>Tentang</h2>
+  <img src="IMG/7.jpeg" class="w3-image w3-padding-32" width="300" height="300">
+  <form style="margin:auto;width:60%" action="/action_page.php" target="_blank">
+    <p>Lukisan adalah suatu hal yang menggambarkan sebuah keindahan yang dapat mempercantik ruangan dan mengisi tempat kosong di ruangan!.</p>
+    <p class="w3-large w3-text-pink">Jika berminat memesan lukisan, silakan isi form dibawah ini!</p>
+    <div class="w3-section">
+      <label><b>Nama</b></label>
+      <input class="w3-input w3-border" type="text" required name="Nama">
+    </div>
+    <div class="w3-section">
+      <label><b>Email</b></label>
+      <input class="w3-input w3-border" type="text" required name="Email">
+    </div>
+    <div class="w3-section">
+      <label><b>Pesan</b></label>
+      <input class="w3-input w3-border" required name="Pesan">
+    </div>
+    <button type="submit" class="w3-button w3-block w3-dark-grey">Send</button>
+  </form>
+  <br>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+</footer>
+ 
+</body>
+</html>
+<?php /**PATH C:\Users\acer\OneDrive\Documents\GitHub\project-laravel\code\AriHeriyadi - 09020582024069\web_statis\resources\views/index.blade.php ENDPATH**/ ?>
