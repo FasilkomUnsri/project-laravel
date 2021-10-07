bcea639ed0c2dfa5a22d02227c172dc1f56e5f33
dellahandayani <90498004+dellahandayani@users.noreply.github.com>
2021-10-07 21:33:05 +0700

    tugass1dellaa

---
+++ code/DellaHandayani-09020582024014/web_statis/resources/views/tugas_della.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <title>Program Web</title>
+    <link rel="stylesheet" type="text/css" href="style/custom.css">
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+             <div class="logo"><a href="">Portofolio</a></div>
+             <div class="menu">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                <ul>
+             </div>
+        </div> 
+    </nav>  
+    <div class="wrapper">
+        <section id="Home">
+            <img src="https://image.freepik.com/free-vector/flat-design-web-developers_23-2148817994.jpg"/>
+            <div class="kolom">
+                <p class="deskripsi">Halo Guys...</p>
+                <h2>Saya Della Handayani</h2>
+                <p>Mahasiswi Jurusan Komputerisasi Akuntansi Universitas Sriwijaya</p>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
