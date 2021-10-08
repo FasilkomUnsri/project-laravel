bb8a33881513ab08c8be77632ead8cc1bddddb55
gustirhy <90498455+gustirhy@users.noreply.github.com>
2021-10-07 19:57:33 -0700

    Tugas_Gusti Rahayu

---
+++ code/GustiRahayu-09020582024012/web_statis/resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Web | Portofolio</title>
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar">
+            <nav>
+                <ul>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Portofolio</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Contact</a></li>   
+                </ul>
+            </nav>
+        </div>
+        <main class="content">
+              <section class="hero">
+                  <img src="../foto/pahawang.png" alt="">
+              <div class="hero-content">
+                  <h1>Wisata Pulau Indonesia</h1> <br> </h2>Pulau Pahawang</h2> <br>
+<p> Pulau pahawang merupakan suatu pulau di indonesia yang terletak di provinsi bandar lampung. Pulau pahawang merupakan suatu destinasi yang sering dikunjungi oleh wisatawan karena tempatnya yang indah. Pulau pahawang juga memiliki keindahan alam bawah laut yang dimana terdapat karang karang dan juga ikan ikan yag lucu dan unik. Apabila ingin melihat keindahan alam bawah laut pulau pahawang harus snorkling.</p>
+<a href="" class="action-btn">Profile Saya</a>
+              </div>
+                </section>
+        </div>
+        <div class="footer">
+            <footer>
+            <ul>
+                <li><img src="../foto/instagram.png" alt=""><p>Instagram</p></a></li>
+                <li><img src="../foto/facebook.png" alt=""><p>Facebook</p></a></li>
+                <li><img src="../foto/twitter.png" alt=""><p>Twitter</p></a></li>
+                <li><img src="../foto/telegram.png" alt=""><p>Telegram</p></a></li>
+            </ul>
+            </footer>
+        </div>
+    </div>    
+</body>
+</html>
