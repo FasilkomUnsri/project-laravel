73b7a35617ca6df752a1189433c3892b0a211dbb
KhairunisaSyahrani <90498552+KhairunisaSyahrani@users.noreply.github.com>
2021-10-08 22:20:35 +0700

    tugas1

---
+++ code/KhairunisaSyahrani-09020582024064/web_statis/resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+        <head>
+            <link rel="stylesheet" href="/style/custom.css">
+        </head>
+ 
+        <body>
+              <div class="nav">
+                <div class="container">
+                    <img src="/style/logo.jpg" class="logo">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">services</a></li>
+                        <li><a href="#">about us </a></li>
+                        <li><a href="#">Blog</a></li>
+                    </ul>
+                </div>
+            </div>
+    
+            <div class="header">
+                <div class="search">
+                    <input type="text" placeholder="search your destination">
+                    <button>Search</button>
+</div>
+            </div>  
+            
+            <div class="footer">
+                <p>khairunisa syahrani</p>
+            </div>
+        </body>
+    <script src="tab.js"></script>
+</html>
