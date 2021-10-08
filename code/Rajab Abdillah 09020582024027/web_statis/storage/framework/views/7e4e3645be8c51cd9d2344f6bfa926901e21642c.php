09a193ed649b68854e704034dd76f9879b085238
RajabAbdillah <90497515+RajabAbdillah@users.noreply.github.com>
2021-10-08 10:12:05 +0700

    tugas

---
+++ code/Rajab Abdillah 09020582024027/web_statis/storage/framework/views/7e4e3645be8c51cd9d2344f6bfa926901e21642c.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="tectical.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+        <div class="sidebar">
+            <nav>
+                <ul>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Portofolio</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Contact</a></li>   
+                </ul>
+            </nav>
+        </div>
+        <main class="content">
+              <section class="hero">
+                  <img src="online.png" alt="">
+              <div class="hero-content">
+                  <h1>Profesi</h1><br></h2>Profil</h2><br><br>   
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, aperiam dolore assumenda velit repellendus recusandae magni consectetur mollitia facere incidunt inventore perspiciatis  debitis doloribus ullam minima culpa voluptatem. Repellendus, option.</p>
+<a href="" class="action-btn">Profile Saya</a>
+              </div>
+                </section>
+        </div>
+        <div class="footer">
+            <footer>
+            <ul>
+                <li><img src="instagram.png" alt=""><p>Instagram</p></a></li>
+                <li><img src="facebook.png" alt=""><p>Facebook</p></a></li>
+            </ul>
+            </footer>
+        </div>
+    </div>  
+</body>
+</html><?php /**PATH C:\Users\Asus\Documents\GitHub\project-laravel\code\Rajab Abdillah 09020582024027\web_statis\resources\views/tectical.blade.php ENDPATH**/ ?>
---
+++ code/Rajab Abdillah 09020582024027/web_statis/storage/framework/views/b808da9d27138c2f6e272ed39c8b42debbea6634.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rajab Abdillah</title>
+    <link rel="stylesheet" href="/style/style.css">
+</head>
+<body>
+    <nav>
+        <div class="wrappaer">
+            <div class="logo"><a href=''>Tectical Store</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#home">home</a></li>
+                    <li><a href="#partners">partners</a></li>
+                    <li><a href="#contact">contact</a></li>
+                    <li><a href="" class="tbl-biru">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <section id="home">
+            <img src="baju.jpg"/>
+            <div class="kolom">
+                <p class="deskripsi">Tectical Store</p>
+                <h2>Tectical Store</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque, magni?</p>
+            </div>
+        </section>
+
+        <section id="courses">
+            <div class="kolom">
+                <p class="deskripsi"
+            </div>
+        </section>
+    </div>
+</body>
+</html><?php /**PATH C:\Users\Asus\Documents\GitHub\project-laravel\code\Rajab Abdillah 09020582024027\web_statis\resources\views/index.blade.php ENDPATH**/ ?>
