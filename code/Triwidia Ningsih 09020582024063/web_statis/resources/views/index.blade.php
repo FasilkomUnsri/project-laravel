b97d20f45433231f9713e1b748107b1b629e6582
triwidia ningsih <triwidianingsih1234@gmail.com>
2021-10-09 09:37:03 +0700

    Tugas Triwidia

---
+++ code/Triwidia Ningsih 09020582024063/web_statis/resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Website Triwidia Ningsih</title>
+</head>
+<body align="center" style="background: lightgreen; color:white;">
+
+    <h1>Cara Pesan Ojek Online Melalui Grab</h1>
+    <h1>Klik Link untuk Memesan</h1>
+
+    <a href="https://www.gojek.com">Grab</a>
+<h3></h3>
+
+    <img style="width: 400px;" src="../foto/wiwid.jpg"><br>
+
+    <h3>Grab merupakan salah satu platform layanan on demand yang bermarkas di Singapura. Berawal dari layanan transportasi, perusahaan tersebut kini telah mempunyai layanan lain seperti pengantaran makanan dan pembayaran yang bisa diakses lewat aplikasi mobile.</h3>
+</body>
+</head>
+</html>
