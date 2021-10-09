fd94fa6a775f5bd041ed76c1c64837032a528c50
revanurrahmadiana <revanrrhmdna11@gmail.com>
2021-10-09 17:09:28 +0700

    Tugas1-WebStatis

---
+++ code/Reva Nur Rahmadiana 09020582024013/WEB_STATIS/resources/views/home.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="widht=device-width, initial-scale=1">
+	<title>REVA NUR RAHMADIANA</title>
+	<link rel="stylesheet" href="style/custom.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<body>
+	<!-- loader -->
+	<div class="bg-loader">
+		<div class="loader"></div>
+	</div>
+	<!-- header -->
+	<div class="medsos">
+		<div class="container">	
+			<ul>
+				<li><a href="https://www.instagram.com/reva.nrrhmdna/"><i class="fab fa-instagram"></i></li>
+			</ul>
+		</div>
+
+	<header>
+		<div class="container">
+			<h1><a href="/home">REVA</a></h1>
+			<ul>
+				<li class="active" ><a href="/home">HOME</a></li>	
+				<li><a href="/about">ABOUT</a></li>
+				<li><a href="/service">SERVICE</a></li>
+				<li><a href="/contac">CONTACT</a></li>	
+			</ul>
+		</div>
+	</header>
+
+	<!-- banner -->
+	<section class="banner">
+		<h2>WELCOME TO MY WEBSITE</h2>
+	</section>
+
+	<!-- about -->
+	<section class="about">
+		<div class="container">
+			<h3>ABOUT</h3>	
+			<p> Hello my name is Reva Nur Rahmadiana. I'm 18 years old. I'm student at University of Sriwijaya. </p>
+		</div>
+	</section>
+
+	<!--service-->
+	<section class="service">
+		<div class="container">
+			<h3>SERVICE</h3>
+			<div class="box">
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-mobile"></i></div>
+					<h4>MOBILE APP</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-globe"></i></div>
+					<h4>WEB DEVELOPMENT</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-edit"></i></div>
+					<h4>DESIGN</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-chart-bar"></i></div>
+					<h4>DIGITAL</h4>
+				</div>
+			</div>
+	</section>
+
+	<!-- footer -->
+	<footer>
+		<div class="container">
+			<small>copyright &copy; 2021 . Reva Nur Rahmadiana. ALL Right Recerved</small>
+		</div>
+	</footer>
+
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$(".bg-loader").hide();
+		})
+	</script>
+</body>
+</html>
