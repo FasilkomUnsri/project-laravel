30a76306ccf62cd3519f1ec57119500ed63c8a41
Marita Santi <maritaati8@gmail.com>
2021-10-18 23:19:56 +0700

    Tugas2-MaritaSanti

---
+++ code/Marita Santi 09020582024019/web_statis2/storage/framework/views/20f6563f3e1b73386acdad5d6625f029be91e32c.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\acer\OneDrive\Dokumen\GitHub\project-laravel\code\Marita Santi 09020582024019\web_statis2\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
