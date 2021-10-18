7280a7e304b22b0e8ccf2c97df2cbf29db5664f5
M Taufik Qurrahman <90497372+Zinu15@users.noreply.github.com>
2021-10-18 22:09:26 +0700

    tugas 2 pembaruan

--- code/MTaufikQurrahman-09020582024026/web_statis2/resources/views/landing.blade.php
+++ code/MTaufikQurrahman-09020582024026/web_statis2/resources/views/landing.blade.php
@@ -13,78 +13,53 @@
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#slider" class="nav-link px-2 text-secondary">Home</a></li>
+                    <li><a href="#hero" class="nav-link px-2 text-secondary">Home</a></li>
                     <li><a href="#about_us" class="nav-link px-2 text-white">About</a></li>
                     <li><a href="#contact_us" class="nav-link px-2 text-white">Contact</a></li>
                 </ul>
-                <div class="text-end">
-                    <a href="#" data-bs-toggle="modal" data-bs-target="#loginModal" class="btn btn-outline-light me-2">Login</a>
-                </div>
             </div>
         </div>
     </header>
     <main>
-        <section class="text-center container-full" id="slider">
-            <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="images/slide/slide1.jpg">           
-                        <div class="container">
-                            <div class="carousel-caption text-start">
-                                <h1>Belajar - M Taufik Qurrahman</h1>
-                                <p>Darinya, aku mengambil filosofi bahwa belajar adalah sikap berani menantang segala ketidakmungkinan; bahwa ilmu yang tak dikuasai akan menjelma di dalam diri manusia menjadi sebuah ketakutan. ...</p>
-                                <p><a class="btn btn-lg btn-primary" href="#">Read More</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/slide/slide2.jpg">
-                        <div class="container">
-                            <div class="carousel-caption">
-                                <h1>Pengetahuan | M Taufik Qurrahman</h1>
-                                <p>Dari semua hal, pengetahuan adalah yang paling baik, karena tidak kena tanggung jawab maupun tidak dapat dicuri, karena tidak dapat dibeli, dan tidak dapat dihancurkan.</p>
-                                <p><a class="btn btn-lg btn-primary" href="#">Read More</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+        <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+            <div class="hero-container" data-aos="fade-in">
+              <h1>Welcome</h1>
+              <p>To <span>MY Portofolio</span></p>
             </div>
-        </section>
+          </section>
         
-        <section id="about_us" class="py-5 px-5">
-            <div class="row ">
-                <div class="col-md-10 order-md-2">
-                    <h2>Halo, Saya <span class="text-muted">M Taufik Qurrahman</span></h2>
-                    <p class="lead">Saya merupakan mahasiswa Jurusan Komputerisasi Akuntansi 2020 dari Fakultas Ilmu Komputer Universitas Sriwijaya.</p>
-                    <h5>Biodata & Riwayat Akademis</h5>
-                    <ul>
-                        <li>M Taufik Qurrahman | mtaufikqurrahman13@gmail.com</li>
-                        <li>SD Negeri 48 Palembang</li>
-                        <li>SMP Negeri 21 Palembang</li>
-                        <li>SMA Methodist 7 Palembang</li>
-                        <li>Universitas Sriwijaya Palembang</li>
-                    </ul>
-                </div>
-                <div class="col-md-2 order-md-1">
-                    <img src="images/about/foto.png" class="img-fluid">
-                </div>
+          <section id="about_us" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Hallo :)</h2>
+          <h2>Saya, M Taufik Qurrahman</h2>
+        </div>
+        <div class="tes">
+            <div class="col-md-2 order-md-1">
+                <img src="images/about/foto1.jpg" class="img-fluid">
             </div>
-        </section>
+        </div>
+        <div class="row">
+            <div class="resume-item pb-0">
+              <p><em>Saya Merupakan Mahasiswa Jurusan Komputer Akuntansi 2020 Dari Fakultas Ilmu Komputer Universitas Sriwijaya.</em></p>
+              <h4>Biodata & Riwayat Akademis</h4>
+              <ul>
+                <li>M Taufik Qurrahman | mtaufikqurrahman13@gmail.com</li>
+                <li>SD Negeri 48 Palembang</li>
+                <li>SMP Negeri 21 Palembang</li>
+                <li>SMA Negeri 7 Palembang</li>
+                <li>Universitas Sriwijaya Palembang</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
 
         <section id="contact_us" class="py-5 px-5 bg-light">
-            <div class="row">
+            <div class="contactus">
                 <div class="col-md-6 py-5 mx-auto">
                     <h4 class="mb-3 text-center">Contact Us</h4>
                     <form>
