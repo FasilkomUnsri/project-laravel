0f9adae9905ebb303b913508c5c4144383ece5cf
Leli Triani <lelitriani992@gmail.com>
2021-10-18 17:02:16 +0700

    web static2

--- code/Lelitriani 09020582024047/web_statis_2/resources/views/landing.blade.php
+++ code/Lelitriani 09020582024047/web_statis_2/resources/views/landing.blade.php
@@ -12,7 +12,7 @@
     <header class="p-3 bg-dark text-white fixed-top">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-
+                
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="#slide" class="nav-link px-2 text-white">Home</a></li>
                     <li><a href="#about_us" class="nav-link px-2 text-white">About</a></li>
