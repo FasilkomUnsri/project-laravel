c3176b198fba2b105f77a57da319541eae286fdd
Muhammad Faza <90498214+faza190302@users.noreply.github.com>
2021-10-20 11:04:31 +0700

    Tugas Web_Statis2 Faza

---
+++ code/Muhammad Faza-09020582024029/web_statis2/resources/views/landing.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-dark">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+
+<body>
+    <h1>Ini Faza</h1>
+</body>
+
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</html>
