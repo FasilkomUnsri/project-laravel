52dc8434d79bedf2ce126917653dcaf5563b489a
teddy792 <90497381+teddy792@users.noreply.github.com>
2021-10-20 21:45:03 +0700

    web_statis_2

---
+++ code/teddy saputra herryanto-09020582024002/web_statis_2/resources/views/index.blade.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="style/style.css"/>
+    <title>Portfolio|Teddy.S.H</title>
+  </head>
+  <body id="home">
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top">
+    
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">TEDDY.S.H</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        <a class="nav-link" href="#about">about me</a>
+        <a class="nav-link" href="#projects">gallery</a>
+        <a class="nav-link" href="#contact">contact me</a>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+
+<section class="jumbotron text-center">
+  <img src="img/fotosaya.jpg" alt="teddy saputra herryanto" width="200" class="rounded-circle img-thumbnail"/>
+  <h1 class="display-4">Teddy Saputra Herryanto</h1>
+  <p class="lead">Mahasiswa|komputerisasi akutansi|universitas srwijaya</p>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,64L48,85.3C96,107,192,149,288,160C384,171,480,149,576,128C672,107,768,85,864,101.3C960,117,1056,171,1152,165.3C1248,160,1344,96,1392,64L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+  
+</section id="about">
+<div class="container">
+  <div class="row text-center mb-3">
+    <div class="col">
+      <h2>tentang saya</h2>
+    </div>
+  </div>
+  <div class="row justify-content-center fs-5 text-center">
+    <div class="col-md-4">
+      <p>perkenalkan nama saya teddy saputra herryanto saya lahir di jambi pada tanggal 7 september 2002 anak ke 3 dari 3 bersudara saya saat ini tinggal di palembang saya lulusan sma methodist 4 angkatan 2020. </p>
+    </div>
+    
+    <div class="col-md-4">
+      <p>saya saat ini melanjutkan pendidikan saya di universitas sriwijaya prodi komputerisasi akutansi pada saat portfolio ini di buat saya telah menempuh 3 semester saya bercita cita untuk memajukan dunia teknologi ke arah yang lebih positif ketika saya sukses nanti. </p>
+
+    </div>
+  </div>
+</div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00bcf5" fill-opacity="1" d="M0,192L48,202.7C96,213,192,235,288,208C384,181,480,107,576,106.7C672,107,768,181,864,218.7C960,256,1056,256,1152,256C1248,256,1344,256,1392,256L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+
+<section>
+</section>
+
+<section id="projects">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col">
+        <h2>Prestasi Saya</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 mb-3">
+      <div class="card">
+  <img src="img/thumbs/10.jpeg" class="card-img-top" alt="project1">
+  <div class="card-body">
+    <p class="card-text">ini adalah saat saya memenangkan medali perak di pertandingan taekwondo tingkat kabupaten di banyuasin dan tim saya mendapatkan juara umum 3.</p>
+  </div>
+</div>
+      </div>
+      <div class="col-md-3 mb-3">
+      <div class="card">
+  <img src="img/thumbs/11.jpeg" class="card-img-top" alt="project2">
+  <div class="card-body">
+    <p class="card-text">ini adalah sertifikat saya sebegai angota paskibra di kecamatan talang kelapa pada tahun 2019 lalu dengan latihan 1 bulan penuh demi mengibarkan bendera merah putih pada 17 agustus 2019 lalu</p>
+  </div>
+</div>
+      </div>
+      <div class="col-md-5 mb-3">
+      <div class="card">
+  <img src="img/thumbs/12.jpeg" class="card-img-top" alt="project3">
+  <div class="card-body">
+    <p class="card-text">ini adalah sertifikat saat saya memenangkan pertandingan taekwondo dan mendapatkan medali perak di piala bupati 2019 lalu dan  team saya mendapat juara 2 umum se kabupaten .</p>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,192L20,176C40,160,80,128,120,112C160,96,200,96,240,117.3C280,139,320,181,360,213.3C400,245,440,267,480,266.7C520,267,560,245,600,208C640,171,680,117,720,117.3C760,117,800,171,840,186.7C880,203,920,181,960,186.7C1000,192,1040,224,1080,224C1120,224,1160,192,1200,165.3C1240,139,1280,117,1320,101.3C1360,85,1400,75,1420,69.3L1440,64L1440,320L1420,320C1400,320,1360,320,1320,320C1280,320,1240,320,1200,320C1160,320,1120,320,1080,320C1040,320,1000,320,960,320C920,320,880,320,840,320C800,320,760,320,720,320C680,320,640,320,600,320C560,320,520,320,480,320C440,320,400,320,360,320C320,320,280,320,240,320C200,320,160,320,120,320C80,320,40,320,20,320L0,320Z"></path></svg>
+
+</section>
+<section id="contact">
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col">
+        <h2>kontak saya </h2>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="mb-3">
+          <label for="Nama" class="form-label">Nama Lengkap</label>
+          <input type="text" class="form-control" id="Nama" aria-describedby="Nama">
+         
+      <div class="col-md-8">
+        <div class="mb-3">
+          <label for="email" class="form-label">Email address</label>
+          <input type="text" class="form-control" id="email" aria-describedby="email">
+     
+      <form>
+  </div>
+  <div class="mb-3">
+  <label for="pesan" class="form-label">pesan</label>
+  <textarea class="form-control" id="pesan" rows="3"></textarea>
+</div>
+  
+  <button type="submit" class="btn btn-primary">kirim</button>
+</form>
+      </div>
+    </div>
+  </div>
+  
+</section>
+
+<footer class="bg-dark text-white text-center p-3">
+  <p>Create By <a href="https://www.instagram.com/teddy.s.herryanto/" class="text-white fw-bold">Teddy saputra Heryanto</a></p>
+
+</footer>
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    
+  </body>
+</html>
