22190d3774c90d5d594239f29bba7ae225eccbdb
Miqbal343 <90497569+Miqbal343@users.noreply.github.com>
2021-10-21 17:47:53 +0700

    Tugas 2 Muhammad Iqbal

---
+++ code/Muhammad Iqbal - 09020582024009/web_statis_2/resources/views/portofoliomuhammadiqbal.blade.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!-- My CSS -->
+    <style>
+    section {
+      min-height: 420px;
+    }
+    </style>
+
+
+
+    <title>My Portfolio!</title>
+  </head>
+  <body class="mt-5">
+    
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+  <a class="navbar-brand" href="#">Muhammad Iqbal</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="container-fuild">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">          
+                <ul class="navbar-nav">
+                    <li><a href="#portfolio" class="nav-link px-2 text-white">Portfolio</a></li>
+                     <li><a href="#about" class="nav-link px-2 text-white">About</a></li>
+                     <li><a href="#contact" class="nav-link px-2 text-white">Contact</a></li>
+                </ul>
+             </div>
+        </div>
+    </div>
+  </div>
+</nav>
+
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container text-center">
+    <img src="img/3.jpg" width="25%" class="rounded-circle img-thumbnail">
+    <h1 class="display-4">Muhammad Iqbal</h1>
+    <p class="lead">Hi!.</p>
+  </div>
+</div>
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <div class="col text-center">
+          <h2>About</h2>
+          </div>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="img/3.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>Mahasiswa.</h3>
+            <p class="fst-italic">
+              Halo! Saya Muhammad Iqbal Mahasiswa Universitas Sriwijaya
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>3 April 2003</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Instagram:</strong> <span>https://www.instagram.com/iqbaall.3/</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+628985991504</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>South Sumatra, Indonesia</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>18</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>mi862234@gmail.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Hobby:</strong> <span>Tidur</span></li>
+                </ul>
+              </div>
+            </div>
+            <p>
+              Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+              Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End About Section -->
+
+<section id="portfolio" class="portfolio bg-light pb-4">
+<div class="container">
+  <div class="row mb-4 pt-4">
+    <div class="col text-center">
+      <h2>Portfolio</h2>
+</div>
+</div>
+
+  <div class="row mb-4">
+    <div class="col-md">
+      <div class="card">
+        <img class="card-img-top" src="img/thumbs/1.jpg" alt="Card image cap">
+        <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+</div>
+    <div class="col-md">
+      <div class="card">
+        <img class="card-img-top" src="img/thumbs/2.jpg" alt="Card image cap">
+        <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+</div>
+    <div class="col-md">
+     <div class="card">
+        <img class="card-img-top" src="img/thumbs/3.jpg" alt="Card image cap">
+        <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+</div>
+</div>
+
+  <div class="row mb-4">
+    <div class="col-md">
+      <div class="card">
+        <img class="card-img-top" src="img/thumbs/4.jpg" alt="Card image cap">
+        <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+</div>
+    <div class="col-md">
+      <div class="card">
+        <img class="card-img-top" src="img/thumbs/55.jpg" alt="Card image cap">
+        <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+</div>
+    <div class="col-md">
+      <div class="card">
+        <img class="card-img-top" src="img/thumbs/6.jpg" alt="Card image cap">
+        <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section id="contact" class="contact mb-5">
+  <div class="container">
+    <div class="row pt-4 mb-4">
+      <div class="col text-center">
+        <h2>Contact Us</h2>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-lg-4">
+        <div class="card text-white bg-primary mb-3 text-center">
+  <div class="card-body">
+    <h5 class="card-title">Contact Us</h5>
+    <p class="card-text"></p>
+  </div>
+</div>
+    <ul class="list-group">
+    <li class="list-group-item"><h1>Location</h1></li>
+    <li class="list-group-item">My Home</li>
+    <li class="list-group-item">Jl. Perintis Kemerdekaan Lr. Produksim</li>
+    <li class="list-group-item">South Sumatra, Indonesia</li>
+</ul>
+      </div>
+
+    <div class="col-lg-6 row pt-4 mb-4">
+    <form>
+      <div class="form-group">
+        <label for="nama">Nama</label>
+        <input type="text" class="form-control" id="nama">
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" class="form-control" id="email">
+      </div>
+      <div class="form-group">
+        <label for="telp">No Telp</label>
+        <input type="text" class="form-control" id="telp">
+      </div>
+      <div class="form-group">
+        <label for="pesan">Pesan</label>
+        <textarea name="pesan" id="pesan" class="form-control"></textarea>
+      </div>
+      <div class="form-group"></div>
+      <button type="button" class="btn btn-primary">Kirim Pesan!</button>
+    </form>
+    </div>
+  </div>
+
+</section>
+
+
+<footer class="bg-dark text-white">
+  <div class="container">
+    <div class="row pt-3">
+      <div class="col text-center">
+        <p>Copyright 2021</p>
+      </div>
+    </div>
+  </div>
+
+</footer>
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
