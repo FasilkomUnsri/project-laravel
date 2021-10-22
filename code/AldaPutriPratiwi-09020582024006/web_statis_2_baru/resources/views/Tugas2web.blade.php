868934e3242630cbbdb5fe6a740a60086bb9fdcb
aldaputripratiwi <aldaputri21@icloud.com>
2021-10-22 20:40:33 +0700

    Tugas web 2

---
+++ code/AldaPutriPratiwi-09020582024006/web_statis_2_baru/resources/views/Tugas2web.blade.php
@@ -0,0 +1,189 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/custom.css">
+<!-- MY CSS -->
+<style >
+    section{
+        min-height: 420px;
+    }   
+    
+</style>
+<script src="https://kit.fontawesome.com/e24321bb28.js" crossorigin="anonymous"></script>
+
+    <title>My Portofolio</title>
+  </head>
+  <body class="mt-5">
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+  <a class="navbar-brand" href="#">ALDA PUTRI PRATIWI</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="#">About</a>
+      <a class="nav-link" href="#">Portofolio</a>
+      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+    </div>
+  </div>
+  </div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container text-center">
+    <img src="style/foto1.jpg" width="15%">
+    <h1 class="display-4">Alda Putri Pratiwi</h1>
+    <p class="lead">Hello! Welcome To My Portofolio .</p>
+  </div>
+</div>
+<section id="About" class="About">
+<div class="container">
+    <div class="row mb-4">
+     <div class="col text-center">
+        <h2>About </h2>
+    </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-5 text-center">
+          <h2> ABOUT ME </h2>
+          <p> My name is Alda Putri Pratiwi As Known As Alda . </p>
+          <p> I'm a Computerize Accounting Students at Sriwijaya University </p>
+          <p> I was born in Palembang 21 of june 2003 </p>
+          <p> I'm 18 years old , I Live at Kebun Bunga st.</p>
+          <p> I have good motivation for growing, eager to learn, and can work in a team and individually. </p>
+        </div>
+        <div class="col-md-5 text-center">
+            <h2> EDUCATIONAL BACKGROUND </h2>
+          <p> 2008– 2014: Elementary School of Muhammadiyah 18 Palembang </p>
+          <p> 2014-2017 : Junior High School of Muhammadiyah 4 Palembang </p>
+          <p> 2017-2020 : Senior High School Of Muhammadiyah 1 Palembang </p>
+          <p> 2020- Now : Diploma Program in Computerized Accounting , Sriwijaya University</p>
+        </div> 
+    </div>
+</div>
+    </section>
+
+ <section id="Portfolio" class="Portfolio bg-light pb-4">
+<div class="container">
+    <div class="row mb-4 pt-4">
+    <div class="col text-center">
+        <h2>Portfolio</h2>
+    </div>
+    </div>
+     <div class="row mb-4">
+        <div class="col-md">
+           <div class="card">
+                <img src="style/fotoalda1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                         <p class="card-text">I participate in dance extracurriculars and I often perform at school event </p>
+                </div>
+            </div>
+        </div>
+            <div class="col-md">
+           <div class="card">
+                <img src="style/fotoalda2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                         <p class="card-text">I love interacting with peoples,I joined the student organization at my school </p>
+                </div>
+            </div>
+        </div>
+            <div class="col-md">
+           <div class="card">
+                <img src="style/himdiko.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                         <p class="card-text">I also joined some students organization at campus , one of them is HIMDIKO </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+<section id="contact" class="contact mb-5">
+<div class="container">
+    <div class="row pt-4">
+        <div class="col text-center">
+            <h2>Contact Us</h2>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-4">
+              <div class="card text-white bg-secondary mb-3">
+              <div class="card-body text-center">
+                <h5 class="card-title">Contact Us</h5>
+                <p class="card-text">
+                  <i class="fab fa-instagram-square"></i> @aldaputripratiwi
+                </p>
+              </div>
+            </div>  
+            <ul class="list-group ">
+              <li class="list-group-item text-center"><h2>Location</h2></li>
+              <li class="list-group-item"> Cempaka Putih 1759 D</li>
+              <li class="list-group-item">Kebun Bunga st.</li>
+              <li class="list-group-item">Palembang,Sumatera Selatan</li>
+            </ul>
+        </div>
+
+
+        <div class="col-lg-6">
+            <form>
+              <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama" placeholder="insert your name">
+              </div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email" placeholder="insert your email">
+              </div>
+              <div class="form-group">
+                <label for="telephone">No Telp</label>
+                <input type="text" class="form-control" id="no telephone" placeholder="insert your phone number">
+              </div>
+              <div class="form-group">
+                <label for="message">Pesan</label>
+               <textarea name="pesan" id="pesan" class="form-control" placeholder="type your message for author"></textarea>
+              </div>
+              <div class="form-grup">
+                  <button type="button" class="btn btn-secondary">Kirim Pesan !</button>
+              </div>
+            </form>
+        </div>
+    </div>
+
+</div>
+</section>
+
+<footer class="bg-dark text-white ">
+    <div class="container">
+        <div class="row text-center pt-3">
+            <div class="col">
+                <p>Copyright_Alda.p.p</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
