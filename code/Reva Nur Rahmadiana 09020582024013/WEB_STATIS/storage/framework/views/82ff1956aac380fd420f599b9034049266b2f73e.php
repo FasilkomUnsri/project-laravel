aa47d99427c6604e05af1087302c427a5c4a4210
revanurrahmadiana <revanrrhmdna11@gmail.com>
2021-10-24 17:37:31 +0700

    Tugas2

---
+++ code/Reva Nur Rahmadiana 09020582024013/WEB_STATIS/storage/framework/views/82ff1956aac380fd420f599b9034049266b2f73e.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Portfolio</title>
+
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="./css/all.css">
+
+
+    <!-- --------- Owl-Carousel ------------------->
+    <link rel="stylesheet" href="./css/owl.carousel.min.css">
+    <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+
+    <!-- ------------ AOS Library ------------------------- -->
+    <link rel="stylesheet" href="./css/aos.css">
+
+    <!-- ------------ Custom Style ------------------------   -->
+    <link rel="stylesheet" href="./css/Style.css">
+
+    <!-- -------------- Favicon -------------------------- -->
+    <link rel="shortcut icon" href="assets/icon.ico">
+
+</head>
+
+<body>
+
+    <!-- ----------------------------  Navigation ---------------------------------------------- -->
+
+    <nav class="nav navbar-fixed-top">
+        <div class="nav-menu flex-row">
+            <div class="nav-brand">
+                <a href="index.html" class="navbar-brand page">REVA NUR RAHMADIANA</a>
+            </div>
+            <div class="toggle-collapse">
+                <div class="toggle-icons">
+                    <i class="fas fa-bars"></i>
+                </div>
+            </div>
+            <div>
+                <ul class="nav-items">
+                    <li class="nav-link">
+                        <a href="/home" class="page-scroll">Home</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="about.html" class="page-scroll">About</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="article.html" class="page-scroll">Article</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="news.html" class="page-scroll">News</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="portfolio.html" class="page-scroll">Portfolio</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="contact.html" class="page-scroll">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="social text-gray">
+                <a href="https://www.instagram.com/reva.nrrhmdna/"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- ------------x---------------  Navigation --------------------------x------------------- -->
+
+    <!----------------------------- Main Site Section ------------------------------>
+
+    <main>
+
+    <!------------------------ Site Title ---------------------->
+
+        <section class="site-title">
+            <div class="site-background" data-aos="fade-up" data-aos-delay="100">
+                <h1>Hi! I'm REVA NUR RAHMADIANA</h1>
+                <h3>UNSRI Fasilkom | Komputerisasi Akuntansi</h3>
+            </div>
+        </section>
+
+        <!------------x----------- Site Title ----------x----------->
+
+
+    <!-- Jquery Library file -->
+    <script src="./js/Jquery3.4.1.min.js"></script>
+
+    <!-- --------- Owl-Carousel js ------------------->
+    <script src="./js/owl.carousel.min.js"></script>
+
+    <!-- ------------ AOS js Library  ------------------------- -->
+    <script src="./js/aos.js"></script>
+
+    <!-- Custom Javascript file -->
+    <script src="./js/main.js"></script>
+    
+</body>
+
+</html><?php /**PATH C:\Users\Acer\Documents\GitHub\project-laravel\code\Reva Nur Rahmadiana 09020582024013\WEB_STATIS\resources\views/index.blade.php ENDPATH**/ ?>
---
+++ code/Reva Nur Rahmadiana 09020582024013/WEB_STATIS2/resources/views/index.blade.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- my css -->
+    <link rel="stylesheet" href="style/style.css">
+    <title>My Portofolio | Reva</title>
+
+</head>
+
+<body id="home">
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning shadow-sm fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Reva Nur Rahmadiana</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#project">Project</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- akhir navbar -->
+    <!-- jumbotron -->
+    <section class="jumbotron text-center">
+        <img src="img/img1.jfif" alt="Reva Nur Rahmadiana" width="200" class="rounded-circle img-thumbnail">
+        <h1 class="display-4">Reva Nur Rahmadiana</h1>
+        <p class="lead">USRI | Fasilkom | Komputerisasi Akuntansi</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffff" fill-opacity="1" d="M0,96L34.3,122.7C68.6,149,137,203,206,229.3C274.3,256,343,256,411,218.7C480,181,549,107,617,106.7C685.7,107,754,181,823,192C891.4,203,960,149,1029,154.7C1097.1,160,1166,224,1234,245.3C1302.9,267,1371,245,1406,234.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+        </svg>
+    </section>
+    <!-- akhir jumbotron -->
+
+    <!-- about -->
+    <section id="about">
+        <div class="about-section">
+            <div class="inner-width">
+                <h1>About Me</h1>
+                <div class="border"></div>
+                <div class="about-section-row">
+                    <div class="about-section-col">
+                        <div class="about">
+                            <img src="img/about.jpg" alt="foto" width="400px">
+                            <p>
+                                Hello my name is Reva Nur Rahmadiana. I'm 18 years old. I'm student at University of Sriwijaya.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        <div class="col">
+                            <h2>My Skill</h2>
+                            <ul id="skill">
+                                <li><span class="expand" style="width:90%;"></span><em>Visual Studio Code</em></li>
+                                <li><span class="expand" style="width:75%;"></span><em>Adobe XD</em></li>
+                                <li><span class="expand" style="width:75%;"></span><em>Corel Draw</em></li>
+                                <li><span class="expand" style="width:80%;"></span><em>Photoshop</em></li>
+                                <li><span class="expand" style="width:95%;"></span><em>Mathematics</em></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="rgb(252, 216, 118)" fill-opacity="1" d="M0,32L40,80C80,128,160,224,240,245.3C320,267,400,213,480,170.7C560,128,640,96,720,112C800,128,880,192,960,192C1040,192,1120,128,1200,96C1280,64,1360,64,1400,64L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+        </svg>
+    </section>
+    <!-- akhir about -->
+    <!-- project -->
+    <section id="project">
+        <div class="container">
+            <div class="row text-center mb-3">
+                <div class="col">
+                    <h2>My Project</h2>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="img/project1.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">project 1</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="img/project2.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">project 2</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="img/project3.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">peoject 3</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffff" fill-opacity="1" d="M0,32L40,80C80,128,160,224,240,245.3C320,267,400,213,480,170.7C560,128,640,96,720,112C800,128,880,192,960,192C1040,192,1120,128,1200,96C1280,64,1360,64,1400,64L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+        </svg>
+    </section>
+    <!-- akhir project -->
+
+    <!-- contact -->
+    <section id="contact">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col">
+                    <h2>
+                        Contact Me
+                    </h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-7">
+                    <form>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nama Lengkap</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="pesan" class="form-label">Pesan</label>
+                            <textarea class="form-control" id="pesan" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Kirim</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffc107" fill-opacity="1" d="M0,224L60,234.7C120,245,240,267,360,256C480,245,600,203,720,202.7C840,203,960,245,1080,240C1200,235,1320,181,1380,154.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+        </svg>
+    </section>
+    <!-- akhir contact -->
+    <!-- footer -->
+    <footer class="bg-warning text-dark text-center p-5">
+        <p>Created by <a href="https://instagram.com/reva.nrrhmdna?utm_medium=copy_link" class="text-dark fw-bold">ig : @Reva Nur Rahmadiana</a></p>
+    </footer>
+    <!-- akhir footer -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
