1de4544499d235d94294018bc2fef988b03df86a
Hgnaa <90498426+Hgnaa@users.noreply.github.com>
2021-11-10 14:47:12 +0700

    web_dinamis

---
+++ code/ReginaTiaraMaharani-09020582024021/web_dinamis/resources/views/artikel/edit.blade.php
@@ -0,0 +1,76 @@
+<!doctype html>
+  <html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Artikel</title>
+    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+</head>
+<body class="d-flex flex-column h-100">
+
+    <main class="flex-shrink-0">
+      <div class="container mt-5">
+        <section id="section_artikel">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card border-0 shadow rounded">
+                        <div class="card-body">
+                            <form action="{{ route('artikel.update', $artikel->id) }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                @method('PUT')
+
+                                <div class="form-group">
+                                    <label class="font-weight-bold">GAMBAR</label>
+                                    <input type="file" class="form-control" name="gambar">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="font-weight-bold">JUDUL</label>
+                                    <input type="text" class="form-control @error('judul') is-invalid @enderror" name="judul" value="{{ old('judul', $artikel->judul) }}" placeholder="Masukkan Judul Artikel">
+
+                                    @error('judul')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group mb-5">
+                                    <label class="font-weight-bold">ISI</label>
+                                    <textarea class="form-control @error('isi') is-invalid @enderror" id="isi" name="isi" rows="5" placeholder="Masukkan Konten Artikel">{{ old('isi', $artikel->isi) }}</textarea>
+
+                                    @error('isi')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+
+                                <button type="submit" class="btn btn-md btn-primary">UPDATE</button>
+                                <button type="reset" class="btn btn-md btn-warning">RESET</button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </div>
+</main>
+
+
+<script src="../../jquery/jquery-3.6.0.min.js"></script>
+<script src="../../bootstrap/js/bootstrap.min.js"></script>
+<script src="../../ckeditor/ckeditor.js"></script>
+
+<script>
+   ClassicEditor
+   .create( document.querySelector( '#isi' ) )
+   .catch( error => {
+    console.error( error );
+} );
+</script>
+
+</body>
+</html>
