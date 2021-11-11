f798b2b0945fb4e7bd28313ddb465001b59f49ae
Selamat Alfisyahrin <90509619+SelamatAlfisyahrin@users.noreply.github.com>
2021-11-11 10:21:08 +0700

    Tugas 3 web Dinamis Selamat Alfisyahrin

--- code/Selamat Alfisyahrin-09020582024020/tugas3/app/Models/Artikel.php
+++ code/Selamat Alfisyahrin-09020582024020/tugas3/app/Models/Artikel.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Artike extends Model
+class Artikel extends Model
 {
     use HasFactory;
-
-    protected $filelable = [
-        'judul'
+    protected $fillable = [
+        'judul', 'isi', 'gambar'
     ];
 }
