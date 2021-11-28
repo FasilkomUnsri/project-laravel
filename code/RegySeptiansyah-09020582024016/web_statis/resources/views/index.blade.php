67467cc66d1047f96711131e8b04ad60f4491218
regyseptiansyah <90956030+regyseptiansyah@users.noreply.github.com>
2021-11-28 16:59:54 +0700

    TUGAS 1
    REGY SEPTIANSYAH

---
+++ code/RegySeptiansyah-09020582024016/web_statis/resources/views/index.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Muhammad Regy Septiansyah</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+<body>
+
+	<!-- header -->
+	<div class="medsos">
+		<div class="container">
+			<ul>
+				<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+				<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+			</ul>
+		</div>
+	</div>
+	<header>
+		<div class="container">
+		<h1><a href="index.html"></a>WELCOME TO MY WEBSITE</h1>
+		<ul>
+			<li class="active"><a href="index.html">HOME</a></li>
+			<li><a href="about.html">PRODUCT</a></li>
+			<li><a href="contact.html">CONTACT</a></li>
+		</ul>
+		</div>
+	</header>
+
+	<!-- banner -->
+	<section class="banner">
+		<h2>WELCOME TO MY WEBSITE</h2>
+	</section>
+
+	<!-- about -->
+	<section class="about">
+		<div class="container">
+			<h3>ABOUT</h3>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <strong>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</strong> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+		</div>
+	</section>
+
+	<!-- footer -->
+	<footer>
+		<div class="container">
+			<small> Copyright &copy; 2021 - Muhammad Regy, All Rights Reserved.</small>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ code/RegySeptiansyah-09020582024016/web_statis2/resources/views/landing.blade.php
@@ -0,0 +1,156 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- My CSS -->
+    <style>
+      section {
+        min-height: 420px;
+      }
+    </style>
+
+    <title>Hallo!</title>
+  </head>
+  <body class="mt-5">
+    
+
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+  <a class="navbar-brand" href="#">M Regy Septiansyah</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">About</a>
+      <a class="nav-item nav-link" href="#">Portfolio</a>
+      <a class="nav-item nav-link disabled" href="#">Contact</a>
+    </div>
+  </div>
+  </div>
+</nav>
+
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container text-center">
+    <img src="gambar/regy.jpeg" width="25%" class="rounded-circle img-thumbnail">
+    <h1 class="display-4">M Regy Septiansyah</h1>
+    <p class="lead">Selamat datang di Komputerisasi Akuntansi UNSRI.</p>
+  </div>
+</div>
+
+<section id="about" class="about">
+<div class="container">
+  <div class="row mb-4">
+    <div class="col text-center">
+      <h2>About</h2>
+    </div>
+  </div>
+
+  <div class="row justify-content-center">
+    <div class="col-md-5 text-center">
+      <p>Perkenalkan nama saya Regy Septiansyah, kalian bisa memanggil saya Regy. Saya mahasiswa Semester 3 dari jurusan Komputerisasi Akuntansi Universitas Sriwijaya</p>
+    </div>
+    <div class="col-md-5 text-center">
+      <p>Saya memiliki hobi bermain game. dan saya mempunyai mimpi menjadi seorang pengusaha sukses yang harus saya wujudkan</p>
+    </div>
+  </div>
+</div>
+</section>
+
+
+<section id="contact" class="contact mb-5">
+<div class="container">
+  <div class="row pt-4 mb">
+    <div class="col text-center">
+      <h2>Kontak Saya</h2>
+    </div>
+  </div>
+
+<div class="row justify-content-center">
+  <div class="col-lg-4">
+    <div class="card text-white bg-primary mb-3 text-center">
+      <div class="card-body">
+        <h5 class="card-title">Kontak Saya</h5>
+        <p class="card-text">No HP:083177255754</p>
+      </div>
+    </div>
+    <ul class="list-group">
+  <li class="list-group-item"><h1>Lokasi Saya<h1></li>
+  <li class="list-group-item">Alamat Rumah</li>
+  <li class="list-group-item">Plaju Silaberanti</li>
+  <li class="list-group-item">Palembang</li>
+</ul>
+  </div>
+
+  <div class="col-lg-6">
+    <form>
+      <div class="form-group">
+        <label for="Nama">Nama</label>
+        <input type="text" class="form-control" id="Nama">
+      </div>
+      <div class="form-group">
+        <label for="Email">Email</label>
+        <input type="text" class="form-control" id="Email">
+      </div>
+      <div class="form-group">
+        <label for="telp">No Telp</label>
+        <input type="text" class="form-control" id="telp">
+      </div>
+      <div class="form-group">
+        <label for="Pesan">Pesan</label>
+        <textarea name="pesan" id="pesan" class="form-control"></textarea>
+      </div>
+      <div class="form-group">
+        <button type="button" class="btn btn-primary">Kirim Pesan</button>
+    </form>
+  </div>
+</div>
+
+
+
+</div>
+</section>
+
+
+
+
+<footer class="bg-dark text-white">
+  <div class="container">
+    <div class="row pt-3">
+      <div class="col text-center">
+        <p>Copyright 2021.</p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
